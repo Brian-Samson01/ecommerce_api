7fe583788689c68deff79380bc8d81ae767a9529
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2026-03-05 10:33:33 +0300

    Virtual environment created and activated  All packages installed  Project and 3 apps created  .env file created with secret key  settings.py configured with DRF + JWT  Server runs without errors

---
+++ ecommerce_api/orders/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Order-related URLs will go here
+]
+
---
+++ ecommerce_api/products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Product-related URLs will go here
+]
+
